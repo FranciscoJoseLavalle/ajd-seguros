29e2ce0919579de19358fb1c693f884ef7140fa3
Francisco José Lavalle <franlavalle@hotmail.com>
2023-03-25 16:09:22 -0300

    Añado seguros momentaneos

--- src/components/Seguros/Seguros.jsx
+++ src/components/Seguros/Seguros.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 const seguros = [
     { label: "Automotores", thumbnail: "./img/seguros/car.png" },
     { label: "Motocicletas", thumbnail: "./img/seguros/motorbike.png" },
-    { label: "Bicicleta / Monopatín", thumbnail: "./img/seguros/bicycle.png" },
+    { label: "Bicicleta/Monopatín", thumbnail: "./img/seguros/bicycle.png" },
     { label: "Combinado Familiar", thumbnail: "./img/seguros/family.png" },
     { label: "Caución", thumbnail: "./img/seguros/handshake.png" },
     { label: "ART", thumbnail: "./img/seguros/helmet.png" },
@@ -23,15 +23,18 @@ const Seguros = () => {
     return (
         <section id='seguros'>
             <div>
-                <h2 data-aos="fade-up">¿Qué <span style={{
-                    color: "#5275e7"
-                }}>ofrecemos?</span></h2>
-                <div className='seguros'>
-                    {seguros.map((seguro, i) => (
-                        <div key={i} className="seguro">
-                            <img src={seguro.thumbnail} alt={seguro.label} />
-                        </div>
-                    ))}
+                <div>
+                    <h2 data-aos="fade-up">¿Qué <span style={{
+                        color: "#5275e7"
+                    }}>ofrecemos?</span></h2>
+                    <div className='seguros'>
+                        {seguros.map((seguro, i) => (
+                            <div key={i} className="seguro" data-aos="flip-left">
+                                <img src={seguro.thumbnail} alt={seguro.label} />
+                                <small>{seguro.label}</small>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </section>
