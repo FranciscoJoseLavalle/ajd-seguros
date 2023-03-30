df4f45334e37cb82200afb3cddcccdc73b80a38a
Francisco José Lavalle <franlavalle@hotmail.com>
2023-03-30 02:13:40 -0300

    Lógica del formulario

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,15 +1,37 @@
 import './Contacto.css';
 import AOS from 'aos'
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
-
+import { useEffect, useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contacto = ({ contactoHeight }) => {
+    const [asunto, setAsunto] = useState('');
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+    const [mensaje, setMensaje] = useState('');
 
     useEffect(() => {
         AOS.init();
     }, [])
+    const sent = (e) => {
+        e.preventDefault();
+        const a = validarCampo(asunto, document.querySelector('#asunto'))
+        const b = validarCampo(nombre, document.querySelector('#nombre'))
+        const c = validarCampo(email, document.querySelector('#email'))
+        const d = validarCampo(mensaje, document.querySelector('#mensaje'))
 
+        if (a && b && c && d) emailjs.send("service_8q90a54", "template_9xc4yyt", { asunto, nombre, email, mensaje }, "DiAaHBMoI1dvusi44");
+        console.log({ asunto, nombre, email, mensaje });
+    }
+    const validarCampo = (campo, input) => {
+        if (/^\s/.test(campo) || campo === '') {
+            input.classList.add('inputWrong');
+            return false
+        } else {
+            input.classList.remove('inputWrong');
+            return true
+        }
+    }
     return (
         <section id='contacto' style={{
             height: contactoHeight
@@ -34,28 +56,28 @@ const Contacto = ({ contactoHeight }) => {
                             </div>
                         </div>
                         <div className='form'>
-                            <form>
+                            <form onSubmit={sent}>
                                 <div className='contact-info'>
                                     <div data-aos="fade-up">
-                                        <label htmlFor='asunto'>Asunto</label>
-                                        <input type="text" id='asunto' placeholder='Ingresa el asunto...' required />
+                                        <label htmlFor='asunto'>* Asunto</label>
+                                        <input type="text" id='asunto' placeholder='Ingresa el asunto...' onChange={(e) => setAsunto(e.target.value)} />
                                     </div>
                                     <div data-aos="fade-up">
-                                        <label htmlFor='nombre'>Nombre</label>
-                                        <input type="text" id='nombre' placeholder='Ingresa tu nombre...' required />
+                                        <label htmlFor='nombre'>* Nombre</label>
+                                        <input type="text" id='nombre' placeholder='Ingresa tu nombre...' onChange={(e) => setNombre(e.target.value)} />
                                     </div>
                                     <div data-aos="fade-up">
-                                        <label htmlFor='email'>Email</label>
-                                        <input type="email" id='email' placeholder='Ingresa tu mail...' required />
+                                        <label htmlFor='email'>* Email</label>
+                                        <input type="email" id='email' placeholder='Ingresa tu mail...' onChange={(e) => setEmail(e.target.value)} />
                                     </div>
                                 </div>
                                 <div data-aos="fade-up">
-                                    <label htmlFor='mensaje'>Mensaje</label>
-                                    <textarea type="text" id='mensaje' placeholder='Ingresa el mensaje...' required />
+                                    <label htmlFor='mensaje'>* Mensaje</label>
+                                    <textarea type="text" id='mensaje' placeholder='Ingresa el mensaje...' onChange={(e) => setMensaje(e.target.value)} />
                                 </div>
                                 <div data-aos="fade-up">
                                     <label htmlFor='archivo'>Suba un archivo</label>
-                                    <input type="file" id='archivo' placeholder='Ingresa el mensaje...' required />
+                                    <input type="file" id='archivo' placeholder='Ingresa el mensaje...' />
                                 </div>
                                 <button>Enviar</button>
                             </form>
