626b8a83ca22b48de6659a26c3d35f9054b62c24
Francisco José Lavalle <franlavalle@hotmail.com>
2023-04-04 13:55:58 -0300

    Cambio el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,26 +6,18 @@ const Footer = () => {
         <footer className='footer'>
             <div>
                 <div className='footer__info'>
-                    <img src="./img/ssn.png" alt="SSN Logo" width={50} height={50} />
-                    <div>
-                        <p>Amir Julián Dib</p>
-                        <small>Productor Asesor de Seguros</small>
-                        <small>Matrícula N°: 98.450</small>
-                        <small>+54 11 3898-5512</small>
-                        <small>seguros.ajd@gmail.com</small>
+                    <div className='footer__info-container'>
+                        <div className='footer__info-img'></div>
+                        <div className='footer__info-text'>
+                            <p>Amir Julián Dib</p>
+                            <small>Productor Asesor de Seguros</small>
+                            <small>Matrícula N°: 98.450</small>
+                            {/* <small>+54 11 3898-5512</small>
+                        <small>seguros.ajd@gmail.com</small> */}
+                        </div>
                     </div>
+                    <img src="./img/compañias/ssn.png" alt="SSN Logo" width={"100%"}/>
                 </div>
-                <nav className='footer__nav'>
-                    <a href="#inicio" className='footer__nav-h1'>
-                        <img src="./img/logo.jpg" alt="Logo" width={50} height={50} />
-                        {/* <h1>AJD<span>Seguros</span></h1> */}
-                    </a>
-                    <ul className='footer__nav-ul'>
-                        {Links.map(link => (
-                            <li key={link.label}><a href={link.route}>{link.label}</a></li>
-                        ))}
-                    </ul>
-                </nav>
             </div>
             {/* <p>Desarrollado por <a href="https://franciscolavalle.netlify.app/" target="_blank">Francisco Lavalle</a></p> */}
         </footer>
@@ -33,3 +25,16 @@ const Footer = () => {
 }
 
 export default Footer
+
+
+// <nav className='footer__nav'>
+// <a href="#inicio" className='footer__nav-h1'>
+//     <img src="./img/logo.jpg" alt="Logo" width={50} height={50} />
+//     {/* <h1>AJD<span>Seguros</span></h1> */}
+// </a>
+// <ul className='footer__nav-ul'>
+//     {Links.map(link => (
+//         <li key={link.label}><a href={link.route}>{link.label}</a></li>
+//     ))}
+// </ul>
+// </nav>
