025a038f7016732aad1b83198797f9ab2eae6c48
Francisco José Lavalle <franlavalle@hotmail.com>
2023-04-24 12:41:53 -0300

    Saco bold en sobre nosotros

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -16,7 +16,8 @@ const Nosotros = () => {
             }}>Nosotros</span></h2>
             <div data-aos="fade-up"
                 data-aos-anchor-placement="center-bottom">
-                <p>Soy un <b>Productor Asesor de Seguros</b> donde mi misión es ofrecerle un <b>seguro</b> acorde a sus necesidades, para así <b>proteger</b> su <b>patrimonio</b> y/o <b>vida</b>.</p>
+                {/* <p>Soy un <b>Productor Asesor de Seguros</b> donde mi misión es ofrecerle un <b>seguro</b> acorde a sus necesidades, para así <b>proteger</b> su <b>patrimonio</b> y/o <b>vida</b>.</p> */}
+                <p>Soy un Productor Asesor de Seguros donde mi misión es ofrecerle un seguro acorde a sus necesidades, para así proteger su patrimonio y/o vida.</p>
             </div>
         </section >
     )
